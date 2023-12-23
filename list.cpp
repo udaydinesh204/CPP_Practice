4b63d1162fa7583b555a5d04937da20c8b5829e3
Uday Dinesh <76675342+udaydinesh204@users.noreply.github.com>
2023-12-23 15:22:26 +0530

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -7,8 +7,8 @@ int main(){
  ls.push_back(3);
  ls.emplace_back(6);//{3,6}
  
- ls.push_fornt(0);//{0,3,6}
- ls.emplace_fornt(-3);
+ ls.push_front(0);//{0,3,6}
+ ls.emplace_front(-3);
  
  //rest functions same as vector ;
 }
