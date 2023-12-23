e7a6b4d06f28f1e4edddf4e99e6f3d72f44a1466
Uday Dinesh <76675342+udaydinesh204@users.noreply.github.com>
2023-12-23 09:52:14 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//return type functions
+void name(){
+  cout<< "uday";
+}
+int sum(int a , int b){
+  return a + b;
+}
+int main() 
+{
+    cout << "Hello, World! \n";
+    int s = sum(3,7);
+    cout << s << endl;
+    name() ;
+    return 0;
+}
